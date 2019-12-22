426d2c3d63938fd3ebe8c5679312c207fc873bdd
fourm2020 <59143437+fourm2020@users.noreply.github.com>
2019-12-22 11:42:16 -0500

    uploading my project

---
+++ forumAutomation_dec21/src/main/java/digitalpoint/forumAutomation_dec21/App.java
@@ -0,0 +1,13 @@
+package digitalpoint.forumAutomation_dec21;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
